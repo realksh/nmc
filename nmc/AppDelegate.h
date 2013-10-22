3b1c0dbbc96d352f12c2de9e09744e7d166580ae
shkim <realk@sk.com>
2013-10-22 12:57:37 +0900

    Initial Commit

---
+++ nmc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nmc
+//
+//  Created by shkim on 13. 10. 22..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ nmc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  nmc
+//
+//  Created by shkim on 13. 10. 22..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
